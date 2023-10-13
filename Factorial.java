e01cb021d8cae5a348e57d096ea2b4b29ce78ba7
yukthi041 <devopsnhc.012@gmail.com>
2023-10-13 14:09:07 +0530

    Factorialofnumber

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial
+{
+public static void main(String args[]){
+int i,n,fact;
+for(i=1;i<=5;i++)
+{
+fact=1;
+n=i;
+while(n>1)
+{
+fact*=n;
+n--;
+}
+System.out.println("Factorial"+i+"="+fact);
+}
+}
+}
+
